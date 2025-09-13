fecea1c9b2137540dc787d7d8fbd65f43b9ad50e
ECW997 <softwarepcecw@gmail.com>
2025-09-13 08:41:45 +0530

    Assign employee to job card job under develop

--- application/controllers/AssignEmployeeToJob.php
+++ application/controllers/AssignEmployeeToJob.php
@@ -10,7 +10,7 @@ class AssignEmployeeToJob extends CI_Controller {
 	public function __construct() {
         parent::__construct();
 		$this->load->helper('api_helper');
-        $this->load->model('JobOptioninfo');
+        $this->load->model('AssignEmployeeToJobinfo');
 
 		$auth_info = auth_check();
 		$this->api_token = $auth_info['api_token'];
@@ -23,70 +23,32 @@ class AssignEmployeeToJob extends CI_Controller {
 		$this->load->view('assign_employee_to_job', $result);
 	}
 
+	public function getJobcardNumbers(){
+		$form_data = [
+			'term' => $this->input->get('term'),
+			'page' => $this->input->get('page'),
+		];
+
+		$response = $this->AssignEmployeeToJobinfo->getJobcardNumbers($this->api_token,$form_data);
+		echo json_encode($response);
+	}
 
 
-
-    // public function index(){
-    //     $this->load->model('AssignEmployeeToJobinfo');
-	// 	$this->load->model('Commeninfo');
-	// 	$result['menuaccess']=$this->Commeninfo->Getmenuprivilege();
-	// 	$result['jobcardlist']=$this->AssignEmployeeToJobinfo->Getjobcard();
-	// 	$result['companylist']=$this->AssignEmployeeToJobinfo->Getcompany();
-	// 	$this->load->view('assign_employee_to_job', $result);
-	// }
-    // public function AssignEmployeeToJobinsertupdate(){
-	// 	$this->load->model('AssignEmployeeToJobinfo');
-    //     $result=$this->AssignEmployeeToJobinfo->AssignEmployeeToJobinsertupdate();
-	// }
-    // public function AssignEmployeeToJobstatus($x, $y){
-	// 	$this->load->model('AssignEmployeeToJobinfo');
-    //     $result=$this->AssignEmployeeToJobinfo->AssignEmployeeToJobstatus($x, $y);
-	// }
-    // public function AssignEmployeeToJobedit(){
-	// 	$this->load->model('AssignEmployeeToJobinfo');
-    //     $result=$this->AssignEmployeeToJobinfo->AssignEmployeeToJobedit();
-	// }
-	// public function getJobList(){
-	// 	$this->load->model('AssignEmployeeToJobinfo');
-    //     $result=$this->AssignEmployeeToJobinfo->getJobList();
-	// }
-	// public function AssignEmployeeToJobView(){
-	// 	$this->load->model('AssignEmployeeToJobinfo');
-    //     $result=$this->AssignEmployeeToJobinfo->AssignEmployeeToJobView();
-	// }
-	// public function department_list_sel2() {
-	// 	if ($this->input->is_ajax_request()) {
-	// 		$page = $this->input->get('page') ?? 1;
-	// 		$company = $this->input->get('company');
-	// 		$term = $this->input->get('term') ?? '';
-
-	// 		$this->load->model('AssignEmployeeToJobinfo');
-
-	// 		$result = $this->AssignEmployeeToJobinfo->department_list_sel2($company, $term, $page);
 	
-	// 		echo json_encode($result);
-	// 	}
-	// }
 
-	// public function supervisor_list_sel2() {
-	// 	if ($this->input->is_ajax_request()) {
-	// 		$page = $this->input->get('page') ?? 1;
-	// 		$department = $this->input->get('department');
-	// 		$term = $this->input->get('term') ?? '';
+	public function getJobcardJobs() {
+    $form_data = [
+        'recordID' => $this->input->post('recordID')
+    ];
 
-	// 		$this->load->model('AssignEmployeeToJobinfo');
+    if (empty($form_data['recordID'])) {
+        echo json_encode(['error' => 'No form data received']);
+        return;
+    }
 
-	// 		$result = $this->AssignEmployeeToJobinfo->supervisor_list_sel2($department, $term, $page);
-	
-	// 		echo json_encode($result);
-	// 	}
-	// }
-	// public function insertupdateSupervisor(){
-	// 	$this->load->model('AssignEmployeeToJobinfo');
-    //     $result=$this->AssignEmployeeToJobinfo->insertupdateSupervisor();
-	// }
-	// public function AssignSupervisorToJobstatus(){
-	// 	$this->load->model('AssignEmployeeToJobinfo');
-    //     $result=$this->AssignEmployeeToJobinfo->AssignSupervisorToJobstatus();
-	// }
+    $response = $this->AssignEmployeeToJobinfo->getJobcardJobs($this->api_token, $form_data);
+    echo json_encode($response);
+}
+
+    
 }
